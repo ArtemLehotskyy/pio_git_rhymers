3a5de464ae70ca91601057722c4dda4f700c8df5
YevhenOleinichenko <252499@edu.p.lodz.pl>
2025-04-08 20:41:08 +0200

    Ad. 8 Annotacja @Override dodana wyłącznie w klasie HanoiRhymer przy metodzie countIn.

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -7,22 +7,18 @@ import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 public class DefaultRhymersFactory implements Rhymersfactory {
 
-    @Override
     public DefaultCountingOutRhymer GetStandardRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
-    @Override
     public DefaultCountingOutRhymer GetFalseRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
-    @Override
     public DefaultCountingOutRhymer GetFIFORhymer() {
         return new FIFORhymer();
     }
 
-    @Override
     public DefaultCountingOutRhymer GetHanoiRhymer() {
         return new HanoiRhymer();
     }
